08f099c68c1a33c172ff43ea37b5ca4e4dc28645
Mario Villaquiran <mariovillaquiran@marios-air.myfiosgateway.com>
2022-03-13 14:02:16 -0400

    brought in bootstrap + worked on nav

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Footer from "./components/Footer";
 import Homepage from "./components/Homepage";
 import AboutMe from "./components/AboutMe";
 import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
 
 function App() {
   return (
--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,13 +1,21 @@
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+
 function NavBar() {
   return (
-    <div>
-      <h2>
-        <a href="">ABOUT ME</a>
-        <a href="">PORTFOLIO</a>
-        <a href="">CONTACT</a>
-        <a href="">RESUME</a>
-      </h2>
-    </div>
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">MARIO VILLAQUIRAN</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="../Homepage">HOME</Nav.Link>
+            <Nav.Link href="../Portfolio">PORTFOLIO</Nav.Link>
+            <Nav.Link href="../Resume">RESUME</Nav.Link>
+            <Nav.Link href="../Contact">CONTACT</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
